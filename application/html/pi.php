bc6ac13d08dac6639dc4e7344499f646e1ece4ee
Francis Crossen <francis.crossen@betbright.com>
2019-03-11 16:42:03 +0000

    Added environmental variables to PHP-FPM; Edited text in pi.php

--- application/html/pi.php
+++ application/html/pi.php
@@ -4,12 +4,11 @@ echo '<h1 style="text-align: center;">' . date('Y-m-d H:i:s') . '</h1>';
 
 echo '<h4 style="text-align: center;">__FILE__ = ' . __FILE__ . '</h4>';
 
-echo "<div>";
-
-echo "Triggering an E_USER_NOTICE error...\r\n";
+echo 'Triggering an E_USER_NOTICE error...<br>';
 trigger_error('User triggered error message', E_USER_NOTICE);
-echo "...done";
+echo '...done.<br><br>';
 
-echo "</div>";
+echo 'Environment variables are exposed for convenience (e.g. database credentials). For example getenv(\'MYSQL_DATABASE\') = ' . getenv('MYSQL_DATABASE') . '<br>';
+echo 'Note that these are exposed by phpinfo() and is therefore a security risk.Environment variables are exposed for convenience (e.g. database credentials). For example getenv(\'MYSQL_DATABASE\') = ' . getenv('MYSQL_DATABASE') . '<br><br>';
 
 echo phpinfo();
